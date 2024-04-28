b72ffb66d81738935dac0727a1ce6fa2e22e74a3
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2024-04-28 02:30:21 -0300

    agrego los pdfs y labo4. aunque raro funciona el calcular ranking. también agrego la pregunta 2

--- funciones.py
+++ funciones.py
@@ -1,5 +1,6 @@
 import numpy as np
 import networkx as nx
+import scipy.linalg as la
 
 
 def leer_archivo(input_file_path):
@@ -17,7 +18,7 @@ def leer_archivo(input_file_path):
     
     return W
 
-def dibujarGrafo(W, print_ejes=True):
+def dibujarGrafo(W, print_ejes=False):
     
     options = {
     'node_color': 'yellow',
@@ -37,14 +38,81 @@ def dibujarGrafo(W, print_ejes=True):
     
     nx.draw(G, pos=nx.spring_layout(G), **options)
 
-def calcularRanking(M, p):
-    npages = M.shape[0]
-    rnk = np.arange(0, npages) # ind{k] = i, la pagina k tienen el iesimo orden en la lista.
+def calcularRanking(W, p):
+    npages = W.shape[0]
+    rnk = np.arange(0, npages) # ind[k] = i, la pagina k tienen el iesimo orden en la lista.
     scr = np.zeros(npages) # scr[k] = alpha, la pagina k tiene un score de alpha 
-    # COIDGO
-    
-    #
+
+    #calculo D
+    D = np.zeros((npages,npages))
+    for i in range(npages):
+        if np.sum(W[i]) == 0:
+            D[i][i] = 0
+        else:
+            D[i][i] = 1/np.sum(W[i])
+    #print(D)
+
+    #armo la matriz M
+    M = np.eye(npages) - p*(W@D)
+
+    #calculo L y U
+    L, U = factorizarLU(M)
+
+    #calculo el score con el sistema de ecuaciones
+    e = np.transpose(np.ones(npages)) # vector de unos, la salida del sistema de ecuaciones
+    y = la.solve_triangular(L, e, lower=True, check_finite=False) # resuelvo Ly=e
+    scr = la.solve_triangular(U, y, check_finite=False) # resuelvo Ux=y
+
+    # normalizo el score
+    scr = scr / np.sum(scr)
+
     return rnk, scr
+
+def factorizarLU(A):
+
+    cant_op = 0
+    m=A.shape[0]
+    n=A.shape[1]
+    Ac = A.copy()
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return
+    
+    #print('Matriz A \n', Ac)
+
+    for n in range(0, m-1): # n es la columna en la que estamos trabajando
+
+        pivot = Ac[n,n]
+        #print("Pivot:", pivot)
+        #print("Estamos trabajando en: \n", Ac[n:,n:])
+
+        for i in range(n+1,m): # i es la fila en la que estamos trabajando
+            
+            #print("Casilla i, n:", Ac[i,n])
+            cociente = Ac[i, n]/pivot # cociente es el valor que se va a restar a la fila i
+            
+            #print("cociente:", cociente)
+            #print("Fila i:", Ac[i, :])
+            #print("Fila n:", Ac[n, :])
+
+            # actualizo la fila i
+            # sólo modificamos los valores de la fila i que están a la derecha de la columna n
+            Ac[i, n:] = Ac[i, n:] - cociente*Ac[n, n:]
+
+            # actualizo la casilla i,n con la modificación que le hicimos a la fila i
+            Ac[i, n] = cociente
+
+            cant_op += 1 # sumo una operación por cada casilla que modificamos
+
+        #print(f'Matriz A ({n+1}) \n', Ac)
+    
+    # obtengo L y U a partir de la matriz Ac modificada por eliminación gaussiana
+    L = np.tril(Ac,-1) + np.eye(A.shape[0]) # matriz triangular inferior con unos en la diagonal
+    U = np.triu(Ac) # matriz triangular superior
+    
+    return L, U #, cant_op
+
 
 def obtenerMaximoRankingScore(M, p):
     output = -np.inf
@@ -55,6 +123,7 @@ def obtenerMaximoRankingScore(M, p):
     return output
 
 
-W = leer_archivo('tests/instagram_famosos_grafo.txt')
+W = leer_archivo('tests/test_dosestrellas.txt')
 
-dibujarGrafo(W)
+#dibujarGrafo(W)
+print(calcularRanking(W, 0.5))
